a9ea28aa1116dbbe3a6a9e6bc2c1913573e59441
t317542 <t317542@t317542-VirtualBox>
2022-01-28 20:39:16 -0300

    retira duplicidade.

--- src/vtr.cpp
+++ src/vtr.cpp
@@ -48,10 +48,8 @@ int main() {
 
 	std::set<std::string>::iterator it;
 	std::pair<std::set<std::string>::iterator, bool> ret;
-
-	std::string str("There are two needles in this haystack with needles.");
-	std::string str2("needle");
 	std::string tjdft("TJDFT");
+	std::string refselo("<referenteSelo>");
 
 	//leituraDeArquivoLinhaAlinha();
 
@@ -65,22 +63,25 @@ int main() {
 		std::cout << "Arquivo " << nomeArquivo << " Aberto" << '\n';
 		for (std::string line; std::getline(is, line);) {
 			std::size_t poselo = line.find(tjdft);
+			std::size_t pref = line.find(refselo);
 			if (poselo != std::string::npos) {
-				setOfSelos.insert(line.substr(poselo, line.npos));
+				if (pref == std::string::npos)
+					setOfSelos.insert(line.substr(poselo, line.npos));
 			}
 		}
 	}
 	is.close();
-	std::cout << "Número de registro no conjunto setOfSelos " << setOfSelos.size() << "\n";
+	std::cout << "Número de registro no conjunto setOfSelos "
+			<< setOfSelos.size() << "\n";
 	if (os.is_open()) {
 		for (auto linhaSelo : setOfSelos) {
-			std::cout
-					<< "update [SIEX].[siex].[SeloDigital] set Status = 'G' where ANO = "
-					<< linhaSelo.substr(5, 4) << "\n"
-					<< " AND CartorioExtrajudicial = " << linhaSelo.substr(9, 3)
-					<< " AND SEQUENCIAL = " << linhaSelo.substr(12, 6)
-					<< " AND DIGITO = '" << linhaSelo.substr(18, 4)
-					<< "' AND Status = 'C' " << "\n";
+			/*std::cout
+			 << "update [SIEX].[siex].[SeloDigital] set Status = 'G' where ANO = "
+			 << linhaSelo.substr(5, 4) << "\n"
+			 << " AND CartorioExtrajudicial = " << linhaSelo.substr(9, 3)
+			 << " AND SEQUENCIAL = " << linhaSelo.substr(12, 6)
+			 << " AND DIGITO = '" << linhaSelo.substr(18, 4)
+			 << "' AND Status = 'C' " << "\n";*/
 			os
 					<< "update [SIEX].[siex].[SeloDigital] set Status = 'G' where ANO = "
 					<< linhaSelo.substr(5, 4) << "\n"
@@ -93,51 +94,32 @@ int main() {
 
 	os.close();
 	// different member versions of find in the same order as above:
-	std::size_t found = str.find(str2);
-	if (found != std::string::npos)
-		std::cout << "first 'needle' found at: " << found << '\n';
-
-	found = str.find("needles are small", found + 1, 6);
-	if (found != std::string::npos)
-		std::cout << "second 'needle' found at: " << found << '\n';
-
-	found = str.find("haystack");
-	if (found != std::string::npos)
-		std::cout << "'haystack' also found at: " << found << '\n';
-
-	found = str.find('.');
-	if (found != std::string::npos)
-		std::cout << "Period found at: " << found << '\n';
-
-	// let's replace the first needle:
-	str.replace(str.find(str2), str2.length(), "preposition");
-	std::cout << str << '\n';
 
 	/*for (std::string line; std::getline(is, line);) {
-		std::cout << line << '\n';
-		std::size_t poselo = line.find(tjdft);
-		if (poselo != std::string::npos)
-			std::cout << "first 'TJDFT' found at: " << poselo << '\n';
+	 std::cout << line << '\n';
+	 std::size_t poselo = line.find(tjdft);
+	 if (poselo != std::string::npos)
+	 std::cout << "first 'TJDFT' found at: " << poselo << '\n';
 
-	}
+	 }
 
-	std::string filename = "Test.b";
-	// prepare a file to read
-	double d = 3.14;
-	std::ofstream(filename, std::ios::binary).write(reinterpret_cast<char*>(&d),
-			sizeof d) << 123 << "abc";
-	// open file for reading
-	std::ifstream istrm(filename, std::ios::binary);
-	if (!istrm.is_open()) {
-		std::cout << "failed to open " << filename << '\n';
-	} else {
-		double d;
-		istrm.read(reinterpret_cast<char*>(&d), sizeof d); // binary input
-		int n;
-		std::string s;
-		if (istrm >> n >> s)                               // text input
-			std::cout << "read back from file: " << d << ' ' << n << ' ' << s
-					<< '\n';
-	}*/
+	 std::string filename = "Test.b";
+	 // prepare a file to read
+	 double d = 3.14;
+	 std::ofstream(filename, std::ios::binary).write(reinterpret_cast<char*>(&d),
+	 sizeof d) << 123 << "abc";
+	 // open file for reading
+	 std::ifstream istrm(filename, std::ios::binary);
+	 if (!istrm.is_open()) {
+	 std::cout << "failed to open " << filename << '\n';
+	 } else {
+	 double d;
+	 istrm.read(reinterpret_cast<char*>(&d), sizeof d); // binary input
+	 int n;
+	 std::string s;
+	 if (istrm >> n >> s)                               // text input
+	 std::cout << "read back from file: " << d << ' ' << n << ' ' << s
+	 << '\n';
+	 }*/
 }
 
